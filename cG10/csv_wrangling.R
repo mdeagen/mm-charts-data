0b0b4488c167e1ce6a071980ba1f7b2a4103dd52
mdeagen <43749866+mdeagen@users.noreply.github.com>
2021-04-13 17:17:20 -0400

    Update csv_wrangling.R

--- cG10/csv_wrangling.R
+++ cG10/csv_wrangling.R
@@ -10,4 +10,15 @@ CTdata$n1 <- str_count(CTdata[,1],"1")
 
 CTdata$norm_density <- (15-CTdata$n1)/15
 
-write.csv(CTdata, file = "CTdata_tidied.csv")
+for(i in 1:nrow(CTdata)) {
+  if(CTdata$norm_density[i] >= 0.8) CTdata$Label[i] <- "A"
+  else if(CTdata$norm_density[i] >= 0.6) CTdata$Label[i] <- "B"
+  else if(CTdata$norm_density[i] >= 0.4) CTdata$Label[i] <- "C"
+  else if(CTdata$norm_density[i] >= 0.2) CTdata$Label[i] <- "D"
+  else CTdata$Label[i] <- "E"
+}
+
+finalData <- CTdata[,c(2:7,9:10)]
+
+write.csv(finalData, file = "CTdata_tidied.csv")
+
